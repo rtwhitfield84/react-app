c36625a43976b0aefb147e32fcbfb8efb7f0e4ba
Rico <richardthomaswhitfield@gmail.com>
2017-03-28 15:38:47 -0500

    state

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import React from 'react';
 
 
 class App extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			txt: 'this is the state text'
+		}
+	}
+	update(e){
+		this.setState({txt: e.target.value})
+	}
 	render(){
-		let txt = this.props.txt
 		return (
 			<div>
-				<h1>{txt}</h1>
-				<b>bold</b>
+			<input type="text"
+			onChange={this.update.bind(this)} />
+				<h1>{this.state.txt}</h1>
 			</div>
 			) 
 	}
-}
-
-App.propTypes = {
-	txt: React.PropTypes.string,
-	cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-	txt:"this is default text",
 }
 
 export default App
