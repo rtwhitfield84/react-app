562ce923002f61859427f0e702c537152bff1dfe
Rico <richardthomaswhitfield@gmail.com>
2017-03-29 13:17:26 -0500

    component updates when new props received

--- src/App.js
+++ src/App.js
@@ -3,52 +3,34 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-	constructor() {
+	constructor(){
 		super();
-		this.state = {val: 0}
-		this.update = this.update.bind(this)
+		this.state = {increasing: false}
 	}
 	update(){
-		this.setState({val: this.state.val + 1})
+		ReactDOM.render(<App val={this.props.val+1} />, 
+			document.getElementById('root'))
 	}
-	componentWillMount(){
-		console.log("componentWillMount");
-		this.setState({m: 2})
+	componentWillReceiveProps(nextProps){
+		this.setState({increasing: nextProps.val > this.props.val})
 	}
-	render(){
-		console.log("render");
-		return <button onClick={this.update}>
-		{this.state.val * this.state.m}</button>
-	}
-	componentDidMount(){
-		console.log("componentDidMount");
-		this.inc = setInterval(this.update, 500)
-	}
-	componentWillUnmount(){
-		console.log("componentWillUnmount");
-		clearInterval(this.inc)
-	}
-}
-
-class Wrapper extends React.Component{
-	mount(){
-		ReactDOM.render(<App />, document.getElementById('a'))
-	}
-	unmount(){
-		ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+	shouldComponentUpdate(nextProps, nextState){
+		return nextProps.val % 5 === 0;
 	}
 	render(){
-		return(
-			<div>
-				<button onClick={this.mount.bind(this)}>Mount</button>
-				<button onClick={this.unmount.bind(this)}>UnMount</button>
-				<div id="a"></div>
-			</div>
-			)
+		console.log(this.state.increasing);
+		return <button onClick={this.update.bind(this)}>
+		{this.props.val}</button>
 	}
+	componentDidUpdate(prevProps, prevState) {
+		console.log(`prevProps ${prevProps.val}`);
+	}
+
 }
 
-export default Wrapper
+App.defaultProps = {val: 0}
+
+export default App
 
 
 
